f46f9df3d7a0fb1fd5f7f403472c6ea4d6463b35
Abhi Kayasth <119026824+Abhikayasth@users.noreply.github.com>
2023-03-08 14:02:25 +0530

    Update Pra_2.java

--- Pra_2.java
+++ Pra_2.java
@@ -1,17 +1,18 @@
-import java.lang.*;
 import java.util.Scanner;
 class Pra_2{
     public static void main(String[] args){
         float BMI;
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enter Weight in Pounds, Height In Inches: ");
-        double pound,inch;
-        pound = sc.nextInt();
-        inch = sc.nextInt();
-        double kg,m;
-        kg = pound * 0.45359237;
-        m = inch * 0.0254;
-        BMI = (kg/(m*m)).floatValue; 
+        System.out.println("Program To Find BMI Index.");
+        float pound,inch;
+        System.out.print("Enter Weight in Pounds :");
+        pound = sc.nextFloat();
+        System.out.print("Enter Height in Inches :");
+        inch = sc.nextFloat();
+        float kg,m;
+        kg = pound * 0.45359237f;
+        m = inch * 0.0254f;
+        BMI = (kg/(m*m)); 
         System.out.println("BMI = "+BMI);
     }
 }
