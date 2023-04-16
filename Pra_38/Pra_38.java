e7965ea785971894d300a63d45cb4c3458e43f85
Kartik Bhatiya <95977507+kartik2433@users.noreply.github.com>
2023-04-16 14:53:22 +0530

    Create Pra_38.java

---
+++ Pra_38/Pra_38.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class sample extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a button and a label
+        Button button = new Button("Click me!");
+        Label label = new Label("Welcome to JavaFx");
+
+        button.setOnAction(event -> {
+            boolean isVisible = label.isVisible();
+            label.setVisible(!isVisible);
+        });
+
+        // Create a VBox layout container to hold the button and label
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.getChildren().addAll(label,button);
+
+        // Create a scene and add the VBox to it
+        Scene scene = new Scene(vbox, 300, 200);
+
+        // Set the scene on the primary stage and show it
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
